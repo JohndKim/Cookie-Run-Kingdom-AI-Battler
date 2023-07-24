70acd296d843fcea847862b6395a7bc4ff42fecb
Kyae <johnkimdw@gmail.com>
2023-07-24 14:49:59 +0800

    update navbar and banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,14 +18,14 @@ function Banner() {
 
       <div className="mt-8 flex flex-wrap justify-center gap-4">
         <a
-          className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-secondary focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
           href="/get-started"
         >
           Get Started
         </a>
 
         <a
-          className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+          className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-secondary focus:outline-none focus:ring active:text-red-500 sm:w-auto"
           href="/about"
         >
           Learn More
