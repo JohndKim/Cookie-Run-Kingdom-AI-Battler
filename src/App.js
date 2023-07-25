5ac3dd13939235bf4ff786c1350f52f48f50e7cd
tealeaf2 <shadowrocket2003@gmail.com>
2023-07-25 20:56:24 +0700

    idk

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import Home from './pages/Home';
-import Battle from './pages/Battle';
-import About from './pages/About';
+// import Home from './pages/Home';
+// import Battle from './pages/Battle';
+// import About from './pages/About';
 
-
-
-// export default function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Banner />
-//     </>
-//   );
-// }
 
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/battle' component={Battle} />
-          <Route path='/about' component={About} />
-        </Switch>
-      </Router>
+      <Navbar />
+      <Banner />
     </>
   );
 }
+
+
+// export default function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Switch>
+//           <Route path='/' exact component={Home} />
+//           <Route path='/battle' component={Battle} />
+//           <Route path='/about' component={About} />
+//         </Switch>
+//       </Router>
+//     </>
+//   );
+// }
