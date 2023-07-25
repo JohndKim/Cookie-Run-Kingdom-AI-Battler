6bb42b0acf1cfebe73c393cf834774c5a302d92e
tealeaf2 <shadowrocket2003@gmail.com>
2023-07-25 21:48:56 +0700

    ewwww

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-import Banner from '../components/Banner';
-import About_Section from '../components/About_Section';
+// import Banner from '../components/Banner';
+import AboutSection from '../components/AboutSection';
 
 export default function About() {
     return ( 
@@ -9,7 +9,7 @@ export default function About() {
         <div className="bg-slate-600">
         {/* <h1>hi</h1> */}
             <Navbar/>      
-            <About_Section/>
+            <AboutSection/>
         </div>
         </>
     );
